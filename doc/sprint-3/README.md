ddf7716e7c2753eb36721ad2df09e80367ac1f32
Adrien LECOMTE <adrien.lecomte@etudiant.univ-lille1.fr>
2017-03-22 17:25:25 +0100

    sprint-3/README.md

---
+++ doc/sprint-3/README.md
@@ -0,0 +1,26 @@
+# Rétrospective de sprint
+
+Nom du scrum master du sprint : Adrien LECOMTE
+
+## Ce que nous avons fait durant ce sprint
+Nous avons finis la géolocalisation et la structure de base. Nous avons compris comment créer nos tables dans la BDD.
+
+## Ce que nous allons faire durant le prochain sprint
+Il faut finir de créer les tables.
+
+## PDCA 
+### Qu'avons nous testé durant ce sprint ? 
+tenté d'utilisé UserDao 
+### Qu'avons nous observé ? 
+Le squelette de base possède pleins de technologies differentes.
+
+### Quelle décision prenons nous suite à cette expérience ? 
+
+### Qu'allons nous tester durant les 2 prochaines heures ? 
+
+
+### À quoi verra-t-on que celà a fonctionné ?
+
+
+# Mémo
+N'oubliez pas d'ajouter une photo du radiateur d'information au moment de la rétrospective.
---
+++ server/src/main/java/fr/iutinfo/skeleton/api/Produit.java
@@ -0,0 +1,147 @@
+package fr.iutinfo.skeleton.api;
+
+import com.google.common.base.Charsets;
+import com.google.common.hash.Hasher;
+import com.google.common.hash.Hashing;
+import fr.iutinfo.skeleton.common.dto.UserDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.security.Principal;
+import java.security.SecureRandom;
+
+public class Produit implements Principal {
+    final static Logger logger = LoggerFactory.getLogger(Produit.class);
+    private static Produit anonymous = new Produit(-1, "Anonymous", "anonym");
+    private int id = 0;
+    private String nom;
+    private float prix;
+    private String description;
+    private String allergies;
+    private int stock=0;
+    private String search;
+
+
+    public Produit(int id, String name) {
+        this.id = id;
+        this.nom = name;
+    }
+
+    public Produit(int id, String name, String description) {
+        this.id = id;
+        this.nom = name;
+        this.description = description;
+    }
+
+    public Produit(String nom, String description, float prix, String allergies, int stock, int id) {
+		super();
+		this.nom = nom;
+		this.description = description;
+		this.prix = prix;
+		this.allergies = allergies;
+		this.stock = stock;
+		this.id = id;
+	}
+
+	public Produit() { }
+
+    public static Produit getAnonymousProduit() {
+        return anonymous;
+    }
+    @Override
+    public boolean equals(Object arg) {
+        if (getClass() != arg.getClass())
+            return false;
+        Produit produit = (Produit) arg;
+        return nom.equals(produit.nom) && description.equals(produit.description) && prix==produit.prix && allergies.equals(produit.allergies) && stock==produit.stock;
+    }
+    @Override
+	public String toString() {
+		return "Produit [id=" + id + ", nom=" + nom + ", prix=" + prix + ", description=" + description + ", allergies="
+				+ allergies + ", stock=" + stock + "]";
+	}
+
+    public boolean isInProduitGroup() {
+        return !(id == anonymous.getId());
+    }
+
+    public boolean isAnonymous() {
+        return this.getId() == getAnonymousProduit().getId();
+    }
+
+    public String getSearch() {
+        search = nom + " " + description + " " ;
+        return search;
+    }
+
+    public void initFromDto(UserDto dto) {
+        this.setId(dto.getId());
+        this.setName(dto.getName());
+    }
+
+    /*public UserDto convertToDto() {
+        UserDto dto = new UserDto();
+        dto.setId(this.getId());
+        dto.setName(this.getName());
+        return dto;
+    }*/
+
+    
+    //**********GETTERS SETTERS**********//
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return nom;
+    }
+
+    public void setName(String name) {
+        this.nom = name;
+    }
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public float getPrix() {
+		return prix;
+	}
+
+	public void setPrix(float prix) {
+		this.prix = prix;
+	}
+
+	public String getAllergies() {
+		return allergies;
+	}
+
+	public void setAllergies(String allergies) {
+		this.allergies = allergies;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+    
+}
