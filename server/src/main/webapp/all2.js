63ce5af2f3ae039029f7d7e9b1f896be49a28d64
plouvinm <plouvinmargot@gmail.com>
2017-03-24 08:30:17 +0100

    inscription

--- server/src/main/webapp/all2.js
+++ server/src/main/webapp/all2.js
@@ -1,25 +1,37 @@
 function inscrire(url) {
- if(document.getElementById("nom").value() != "" && document.getElementById("adresse").value() != "" && 
-		 document.getElementById("mdp").value() != "" && document.getElementById("prenom").value() != "" && 
-		 document.getElementById("mail").value() != "" && document.getElementById("mdp2").value( != "") {
-     $.ajax
-     ({
-       type: "POST",
-       url: url,
-       dataType: 'json',
-       beforeSend : function(req) {
-        req.setRequestHeader("Authorization", "Basic " + btoa($("#userlogin").val() + ":" + $("#passwdlogin").val()));
-       },
-       success: function (data) {
-        afficheUser(data);
-       },
-       error : function(jqXHR, textStatus, errorThrown) {
-       			alert('error: ' + textStatus);
-       		}
-     });
-     } else {
-     $.getJSON(url, function(data) {
-     	    afficheUser(data);
-        });
+ if($("#nom").val().length>0 && $("#adresse").val().length>0 && $("#mdp").val().length>0 && $("#prenom").val().length>0 && 
+		 $("#mail").val().length>0 && $("#mdp2").val().length>0) {
+   
+	 var name = $("#nom").val();
+	 var adresse = $("#adresse").val();
+	 var mdp = $("#mdp").val();
+	 var prenom = $("#prenom").val();
+	 var mail = $("#mail").val();
+	 
+	 
+	 
+	 $.ajax({
+				type : "POST",
+				url : url,
+				contentType : "application/json",
+				data : JSON.stringify({
+					"nom" : name,
+					"adresse" : adresse,
+					"mdp" : mdp,
+					"prenom" : prenom,
+					"mail" : mail,
+				}),
+				success : function(data) {
+					alert('vous êtes bien inscrit');
+
+				},
+				error : function(jqXHR, textStatus, errorThrown) {
+					alert('error: ' + textStatus);
+				}
+			});
+		} else {
+			alert("veuillez remplir tous les champs !");
+		}
+
      }
- }
+ 
