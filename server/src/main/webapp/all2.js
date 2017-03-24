487842f19b5aeff7d53d0a64fb21495727cb74bd
plouvinm <plouvinmargot@gmail.com>
2017-03-24 10:31:55 +0100

    ajout modif plats

--- server/src/main/webapp/all2.js
+++ server/src/main/webapp/all2.js
@@ -1,37 +1,67 @@
 function inscrire(url) {
- if($("#nom").val().length>0 && $("#adresse").val().length>0 && $("#mdp").val().length>0 && $("#prenom").val().length>0 && 
-		 $("#mail").val().length>0 && $("#mdp2").val().length>0) {
-   
-	 var name = $("#nom").val();
-	 var adresse = $("#adresse").val();
-	 var mdp = $("#mdp").val();
-	 var prenom = $("#prenom").val();
-	 var mail = $("#mail").val();
-	 
-	 
-	 
-	 $.ajax({
-				type : "POST",
-				url : url,
-				contentType : "application/json",
-				data : JSON.stringify({
-					"nom" : name,
-					"adresse" : adresse,
-					"mdp" : mdp,
-					"prenom" : prenom,
-					"mail" : mail,
-				}),
-				success : function(data) {
-					alert('vous êtes bien inscrit');
+	if($("#nom").val().length>0 && $("#adresse").val().length>0 && $("#mdp").val().length>0 && $("#prenom").val().length>0 && 
+			$("#mail").val().length>0 && $("#mdp2").val().length>0) {
 
-				},
-				error : function(jqXHR, textStatus, errorThrown) {
-					alert('error: ' + textStatus);
-				}
-			});
-		} else {
-			alert("veuillez remplir tous les champs !");
-		}
+		var name = $("#nom").val();
+		var adresse = $("#adresse").val();
+		var mdp = $("#mdp").val();
+		var prenom = $("#prenom").val();
+		var mail = $("#mail").val();
 
-     }
- 
+
+
+		$.ajax({
+			type : "POST",
+			url : url,
+			contentType : "application/json",
+			data : JSON.stringify({
+				"nom" : name,
+				"adresse" : adresse,
+				"mdp" : mdp,
+				"prenom" : prenom,
+				"mail" : mail,
+			}),
+			success : function(data) {
+				alert("vous êtes bien inscrit");
+
+			},
+			error : function(jqXHR, textStatus, errorThrown) {
+				alert('error: ' + textStatus);
+			}
+		});
+	} else {
+		alert("veuillez remplir tous les champs !");
+	}
+
+}
+
+
+
+function connexion(url) {
+	if($("#loginUser").val().length>0 ) {
+		var nameConnexion = $("#login").val();
+
+		$.ajax({
+			type : "GET",
+			url : "v1/user",
+			success : function(data) {
+				$("#zone").text("");
+				data.forEach(function(element) {
+					$("#zone").append(element.name + "<br />");
+				});
+
+			},
+			error : function(jqXHR, textStatus, errorThrown) {
+				alert('error: ' + textStatus);
+			}
+		});
+
+	} else {
+		alert("veuillez remplir tous les champs !");
+	}
+}
+
+function ajoutProduit(url){
+	if($("#nomProduit").val().length>0 && $("#descriptionForm").val().length>0 && $("#prixForm").val().length>0 && $("#allergenesForm").val().length>0 && 
+			$("#qteForm").val().length>0 && $("#imageForm").val().length>0) {
+}
