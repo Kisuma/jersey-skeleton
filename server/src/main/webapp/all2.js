19019b3c72354476079ac19f0deae18d49d955ad
Guillaume BETTE <guillaume.bette@etudiant.univ-lille1.fr>
2017-03-27 10:56:18 +0200

    modif pdt

--- server/src/main/webapp/all2.js
+++ server/src/main/webapp/all2.js
@@ -121,39 +121,6 @@ function listerUsers(url) {
 	});
 }
 
-function listerSupplements(url) {
-	$("#supplements").text("chargement...");
-
-	$.ajax({
-		type : "GET",
-		url : url,
-		success : function(data) {
-	$("#supplements").text("");
-	var s = "<div id=\"MainContainer\" class=\"container-fluid\" style=\"margin-top=60px;\" >";
-	var j = "http://www.supermarchesmatchdrive.fr/media/catalog/product/cache/1/image/300x/9df78eab33525d08d6e5fb8d27136e95/1/3/1365428_37901.jpg";
-	data.forEach(function(element) {
-		if(element.supplement){
-			s+= "<div class=\"row\" > <div class =\"col-sm-6 col-md-3\"> <div class=\"thumbnail\" style=\"text-align:center;\">";
-			s+= "<a href=\"#\" > <img src=\" " + j+ "\"></a>";
-			s+= "<div class=\"caption\">";
-			s+= "<h3>" + element.name + "</h3>"
-			s+= "<p>" + element.prix + "</p>";
-			s+= "<p>" + element.allergies + "</p>";
-			s+= "<p><a id =\"btnDetails\" href=\"#\" class=\"btn btn-default\" role=\"button\">Détails</a>";
-			s+= "<a href=\"#\" class=\"btn btn-primary\" role=\"button\">Ajouter</a></p></div></div></div>";
-		}
-	});
-	s += "</div>";
-	$("#supplements").append(s);
-
-
-	},
-	error : function(jqXHR, textStatus, errorThrown) {
-		alert('error: ' + textStatus);
-	}
-});
-}
-
 function listerProduits(url) {
 	$("#menus").text("chargement...");
 
@@ -162,10 +129,10 @@ function listerProduits(url) {
 		url : url,
 		success : function(data) {
 			$("#menus").text("");
-			var s = "<div id=\"MainContainer\" class=\"container-fluid\" style=\"margin-top=60px;\" >";
+			var s = "<h1 style=\"margin-left:60px;\"> Les Menus et les suppléments</h1><div id=\"MainContainer\" class=\"container-fluid\" style=\"margin-top:60px;\" >";
 			var j = "http://www.supermarchesmatchdrive.fr/media/catalog/product/cache/1/image/300x/9df78eab33525d08d6e5fb8d27136e95/1/3/1365428_37901.jpg";
 			data.forEach(function(element) {
-				if(!element.supplement){
+				
 				s+= "<div class=\"row\" > <div class =\"col-sm-6 col-md-3\"> <div class=\"thumbnail\" style=\"text-align:center;\">";
 				s+= "<a href=\"#\" > <img src=\" " + j+ "\"></a>";
 				s+= "<div class=\"caption\">";
@@ -174,19 +141,7 @@ function listerProduits(url) {
 				s+= "<p>" + element.allergies + "</p>";
 				s+= "<p><a id =\"btnDetails\" href=\"#\" class=\"btn btn-default\" role=\"button\">Détails</a>";
 				s+= "<a href=\"#\" class=\"btn btn-primary\" role=\"button\">Ajouter</a></p></div></div></div>";
-				}
-				/*	
-				 s += "<tr><td>Nom : " + element.name + "</td><br /><td> Prix : " + element.prix + "</td><br /><td> Desctription du produit : " 
-					     + element.description + "</td><br /><td> Allergies : " + element.allergies + "</td><br /><td> Nous avons en stock " 
-					     + element.stock + " " + element.name+ "</td><br /><td>" 
-					     + "<img src='http://www.supermarchesmatchdrive.fr/media/catalog/product/cache/1/image/300x/9df78eab33525d08d6e5fb8d27136e95/1/3/1365428_37901.jpg'></td> <br />"
-					     + "</tr><br />";
-
-				$("#zone").append("<tr><td> Nom : " + element.name + "<br />Prix : " + element.prix + "<br />Desctription du produit : " 
-					     + element.description + "<br />Allergies : " + element.allergies + "<br />Nous avons en stock " 
-					     + element.stock + " " + element.name+ "<br />" 
-					  //   + "<img src='http://www.supermarchesmatchdrive.fr/media/catalog/product/cache/1/image/300x/9df78eab33525d08d6e5fb8d27136e95/1/3/1365428_37901.jpg'> <br />"
-					     + "</tr></td>");*/
+				
 			});
 			s += "</div>";
 			$("#menus").append(s);
